20b4b6d2a172c5b6b9b03dc8da8801589b7109dd
Kristian Kjeldsen <jg@renegade.se>
2023-01-09 12:11:22 +0100

    added sqlite

---
+++ SqliteDataAccess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+
+namespace file_io
+{
+	public class SqliteDataAccess
+	{
+		public List<UserModel> LoadUsers()
+		{
+			//
+		}
+		private static string LoadConnectionString(string id = "Default")
+		{
+			return ConfigurationManager.ConnectionStrings
+		}
+		
+	}
+}
+
---
+++ UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace file_io
+{
+	public class UserModel
+	{
+		public int Id { get; set; }
+
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public string PinCode { get; set; }
+
+		public string FullName
+		{
+			get
+			{
+				return $"{FirstName} {LastName}";
+			}
+		}
+	}
+}
+
